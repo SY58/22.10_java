fff3b1447a77670b701ad79f44f0ea8ee42aa648
syCHOI <sychoi.me@gmail.com>
2022-11-02 18:17:39 +0900

    Step14_Swing 프로젝트에 frame 예제 추가

--- Step14_Swing/src/frame05/MyFrame.java
+++ Step14_Swing/src/frame05/MyFrame.java
@@ -1,18 +1,26 @@
 package frame05;
 
-import java.awt.BorderLayout;
+
+import java.awt.Component;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class MyFrame extends JFrame{
 	//생성자
 	public MyFrame(String title) {
 		super(title);
+		
+		//this 예약어를 이용해서 MyFrame 객체의 다형성 확인!
+		MyFrame a=this;
+		JFrame b=this;
+		Component c=this;
+		Object d=this;
+		
 		setBounds(100,100,500,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new FlowLayout());
@@ -24,10 +32,36 @@ public class MyFrame extends JFrame{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("전송 버튼을 눌렀네?");
+				
+				JOptionPane.showMessageDialog(MyFrame.this, "전송합니다");
+				
+				ActionListener f=this;
+				//Component f=this; (X) 
+				Component g=MyFrame.this; 
 			}			
 		};
 		//전송버튼에 등록하기
 		sendBtn.addActionListener(listener);
+		
+		/*
+		 * 삭제 버튼도 프레임에 배치하고, 해당 버튼을 눌렀을 때 "삭제 버튼을 눌렀네?"라는
+		 * 메세지를 콘솔창에 출력되도록 프로그래밍 해보세요.
+		 */
+		JButton deleteBtn=new JButton("삭제");
+		add(deleteBtn);		
+		ActionListener listener2=new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("삭제 버튼을 눌렀네?");
+				JOptionPane.showMessageDialog(MyFrame.this, "삭제합니다");
+			}						
+			
+		};
+		/* deleteBtn.addActionListener((msg)->
+		*	System.out.println("삭제 버튼을 눌렀네?"));
+		*/
+		deleteBtn.addActionListener(listener2);	
+
 	}
 	
 	public static void main(String[] args) {
